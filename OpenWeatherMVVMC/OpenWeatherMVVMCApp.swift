38a550a11f614f7fa8ad66d57ecc706d2b76dfb2
Quan <quanrong1988@yahoo.com.vn>
2023-06-03 11:35:51 +0700

    Initial Commit

---
+++ OpenWeatherMVVMC/OpenWeatherMVVMCApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherMVVMCApp.swift
+//  OpenWeatherMVVMC
+//
+//  Created by Tạ Minh Quân on 03/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherMVVMCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
